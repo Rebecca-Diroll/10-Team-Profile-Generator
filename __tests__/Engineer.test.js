7e6c5bb96d331a57c8ae4027bb3c2d295dc0671d
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-15 11:01:26 -0400

    Add engineer test code

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,16 @@
+const Engineer = require("../models/EngineerClass");
+
+describe("Engineer", () => {
+
+    const Engineer = new Engineer("Dan", "03", "dan@email.com", "gitDan");
+    
+        it("returns Engineer role", () => {
+            let role = engineer.getRole();
+            expect(role).toBe("Engineer");
+        });
+
+        it("returns github user name", () => {
+            let github = engineer.getGithub();
+            expect(github).toBe("gitDan");
+        });
+});
